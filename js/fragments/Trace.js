faf4b8d32b9693d87b32983845918645409141c8
unknown <nami0879@naver.com>
2024-01-03 01:39:37 +0900

    trace

---
+++ js/fragments/Trace.js
@@ -0,0 +1,36 @@
+import { ctx } from '../canvas.js';
+import Fragment from './fragment.js';
+
+export default class Trace {
+    constructor(x, y, objectWidth, objectHeight, objectColor) {
+        this.milliseconds = 3 * 1000;
+        this.startTime = performance.now();
+
+        this.x = x;
+        this.y = y;
+        this.objectWidth = objectWidth;
+        this.objectHeight = objectHeight;
+        this.objectColor = objectColor;
+    }
+
+    update() {
+        const currentTime = performance.now();
+        const elapsed = currentTime - this.startTime;
+        this.progress = Math.min(elapsed / this.milliseconds, 1);
+    }
+
+    draw() {
+        if (this.progress >= 1) {
+
+            // 현재 Particle 객체를 fragmentArr 배열에서 찾아서 삭제
+            const index = Fragment.fragmentArr.indexOf(this);
+            Fragment.fragmentArr.splice(index, 1);
+
+            return;
+        }
+
+        ctx.fillStyle = this.objectColor;
+        ctx.globalAlpha = 0.1;
+        ctx.fillRect(this.x, this.y, this.objectWidth, this.objectHeight);
+    }
+}
